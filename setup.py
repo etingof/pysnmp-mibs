91264248ecc4d5e8d2cd0e9170cad55a9368d605
elie <elie>
2004-10-29 13:52:53 +0000

    updated to reflect latest package layout

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name="pysnmp-mibs",
-      version="0.0.1",
+      version="0.1.1",
       description="A collection of SNMP MIBs for PySNMP toolkit",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
-      packages = [ 'pysnmp_mibs',
-                   'pysnmp_mibs.libsmi' ],
+      data_files=[('/usr/local/bin',
+                   ['tools/libsmi2pysnmp', 'tools/pysnmpmibdir'])],
       license="BSD")
