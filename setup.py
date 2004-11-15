dbf2de8096dccb5cfc9904dda213bde3365d0f08
elie <elie>
2004-11-15 13:02:54 +0000

    desc fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name="pysnmp-mibs",
       version="0.0.1a",
-      description="A collection of SNMP MIBs for PySNMP toolkit",
+      description="A collection of pre-compiled SNMP MIBs for PySNMP",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
