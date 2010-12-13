24e2d6815d791986183f5ef4a1e0a626a349f1a1
elie <elie>
2010-12-13 15:51:31 +0000

    compatibility fixes for better setuptools/distutils compatibility

--- setup.py
+++ setup.py
@@ -12,20 +12,33 @@ It's very easy to install it, just type (as root on Linux):
 
 try:
     from setuptools import setup
+    params = {
+        'install_requires': [ 'pysnmp' ],
+        'zip_safe': True
+        }
 except ImportError:
     for arg in sys.argv:
         if string.find(arg, 'egg') != -1:
             howto_install_setuptools()
             sys.exit(1)
     from distutils.core import setup
+    if sys.version_info > (2, 2):
+        params = {
+            'requires': [ 'pysnmp' ]
+            }
+    else:
+        params = {}
 
-setup(name = 'pysnmp-mibs',
-      version = '0.0.9a',
-      description = 'A collection of pre-compiled PySNMP MIBs',
-      author = 'Ilya Etingof',
-      author_email = 'ilya@glas.net',
-      url = 'http://sourceforge.net/projects/pysnmp/',
-      license = 'BSD',
-      requires = [ 'pysnmp' ],
-      packages = [ 'pysnmp_mibs' ],
-      scripts = [ 'tools/rebuild-pysnmp-mibs' ])
+params.update( {
+    'name': 'pysnmp-mibs',
+    'version': '0.0.10a',
+    'description': 'A collection of pre-compiled PySNMP MIBs',
+    'author': 'Ilya Etingof',
+    'author_email': 'ilya@glas.net',
+    'url': 'http://sourceforge.net/projects/pysnmp/',
+    'license': 'BSD',
+    'packages': [ 'pysnmp_mibs' ],
+    'scripts': [ 'tools/rebuild-pysnmp-mibs' ]
+    } )
+               
+apply(setup, (), params)
