40c27d1bc3b1494d4b34aaec508bbd8fb7b25641
elie <elie>
2011-11-06 21:58:08 +0000

    MIB modules re-generated from text source with Python 3 and updated, Py3k-compliant, libsmi2pysnmp tool.

--- setup.py
+++ setup.py
@@ -1,44 +1,54 @@
 #!/usr/bin/env python
 import sys
-import string
 
 def howto_install_setuptools():
-    print """Error: You need setuptools Python package!
+    print("""Error: You need setuptools Python package!
 
 It's very easy to install it, just type (as root on Linux):
    wget http://peak.telecommunity.com/dist/ez_setup.py
    python ez_setup.py
-"""
+""")
 
 try:
     from setuptools import setup
     params = {
-        'install_requires': [ 'pysnmp' ],
+        'install_requires': [ 'pysnmp>=4.2.1' ],
         'zip_safe': True
         }
 except ImportError:
     for arg in sys.argv:
-        if string.find(arg, 'egg') != -1:
+        if arg.find('egg') != -1:
             howto_install_setuptools()
             sys.exit(1)
     from distutils.core import setup
-    if sys.version_info > (2, 2):
-        params = {
-            'requires': [ 'pysnmp' ]
-            }
-    else:
-        params = {}
+    params = {
+        'requires': [ 'pysnmp(>=4.2.1)' ]
+        }
 
 params.update( {
     'name': 'pysnmp-mibs',
-    'version': '0.0.10a',
-    'description': 'A collection of pre-compiled PySNMP MIBs',
+    'version': '0.1.1',
+    'description': 'A collection of IETF & IANA MIBs pre-compiled for PySNMP',
     'author': 'Ilya Etingof',
     'author_email': 'ilya@glas.net',
     'url': 'http://sourceforge.net/projects/pysnmp/',
+    'classifiers': [
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Information Technology',
+      'Intended Audience :: Telecommunications Industry',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 3',
+      'Topic :: Communications',
+      'Topic :: System :: Monitoring',
+      'Topic :: System :: Networking :: Monitoring',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'License :: OSI Approved :: BSD License'
+    ],    
     'license': 'BSD',
     'packages': [ 'pysnmp_mibs' ],
     'scripts': [ 'tools/rebuild-pysnmp-mibs' ]
     } )
                
-apply(setup, (), params)
+setup(**params)
