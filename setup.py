8a364bae72a64f2cdeac5f9f0b742bf6bf896079
elie <elie>
2015-10-10 18:41:56 +0000

    fix to __doc__ use in setup.py to make -O0 installation mode working

--- setup.py
+++ setup.py
@@ -43,10 +43,8 @@ if sys.version_info[:2] < (2, 4):
 
 try:
     from setuptools import setup
-    params = {
-        'install_requires': [ 'pysnmp>=4.3.0' ],
-        'zip_safe': True
-        }
+    params = {'install_requires': ['pysnmp>=4.3.0'],
+              'zip_safe': True}
 except ImportError:
     import sys
     for arg in sys.argv:
@@ -58,21 +56,21 @@ except ImportError:
     if sys.version_info[:2] > (2, 4):
         params['requires'] = [ 'pysnmp(>=4.3.0)' ]
 
-doclines = [ x.strip() for x in __doc__.split('\n') if x ]
+doclines = [x.strip() for x in (__doc__ or '').split('\n') if x]
 
-params.update( {
-    'name': 'pysnmp-mibs',
-    'version': open('pysnmp_mibs/__init__.py').read().split('\'')[1],
-    'description': doclines[0],
-    'long_description': ' '.join(doclines[1:]),
-    'maintainer': 'Ilya Etingof <ilya@glas.net>',
-    'author': 'Ilya Etingof',
-    'author_email': 'ilya@glas.net',
-    'url': 'http://sourceforge.net/projects/pysnmp/',
-    'classifiers': [ x for x in classifiers.split('\n') if x ],
-    'platforms': ['any'],
-    'license': 'BSD',
-    'packages': [ 'pysnmp_mibs' ]
-    } )
+params.update(
+    {'name': 'pysnmp-mibs',
+     'version': open('pysnmp_mibs/__init__.py').read().split('\'')[1],
+     'description': doclines[0],
+     'long_description': ' '.join(doclines[1:]),
+     'maintainer': 'Ilya Etingof <ilya@glas.net>',
+     'author': 'Ilya Etingof',
+     'author_email': 'ilya@glas.net',
+     'url': 'http://sourceforge.net/projects/pysnmp/',
+     'classifiers': [x for x in classifiers.split('\n') if x],
+     'platforms': ['any'],
+     'license': 'BSD',
+     'packages': ['pysnmp_mibs']}
+)
                
 setup(**params)
